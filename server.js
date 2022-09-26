93abeb088adc5514c636e89a92114b577c91a151
Robert Ford <robert.ford1996@gmail.com>
2022-09-26 09:49:14 -0400

    dependencies

---
+++ server.js
@@ -0,0 +1,21 @@
+//DEPENDENCIES
+
+//get .env variables
+require("dotenv").config();
+//pull PORT from .env, give default value of 4000
+const { PORT = 4000 } = process.env;
+//import express
+const express = require("express");
+//create application object
+const app = express();
+
+//ROUTES
+
+//test route
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+//LISTENER
+
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
